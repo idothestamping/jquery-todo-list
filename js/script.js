35d78d259c8ade3a9c718fab8f4ff99d14581405
idothestamping <douglas.klemp@gmail.com>
2018-12-05 09:01:28 -0800

    ToDoList classRoom exercise

---
+++ js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function(){
+	console.log("DOMContentLoaded");
+
+
+
+console.log('linked');
+
+
+$('#buttonAdd').click(function(){
+	var contents = $('#userInputName').val()
+	$('ul').append('<li>' + contents + '</li>')
+})
+
+$('#buttonRemove').click(function(){
+	//var removeList = $('ul').val()
+	$('.mainList li:last-child').remove();
+});
+
+
+// $('li').hover(function(){
+// 	console.log('hover in');
+// 	$(this).css('background-color', 'red');
+// }, function(){
+// 	console.log('hover out');
+// 	$(this).css('background-color', '');
+	
+$(document).on('dblclick','li', function(){
+	$(this).toggleClass('strike').fadeOut('slow');    
+});
+
+$('#userInputName').focus(function() {
+$(this).val('');
+});
+
+$('li').sortable();  
+
+
+});
+
+
+
+
+
